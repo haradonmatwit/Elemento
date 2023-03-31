268e6ecaad0cfe818efbb97d6e564ed78139f543
ahmedsatwit <106392510+ahmedsatwit@users.noreply.github.com>
2023-03-31 14:00:33 -0400

    some organization

--- feedbackForm/mysql.sql
+++
@@ -1,9 +0,0 @@
--- CREATE TABLE emp (empid INTEGER NOT NULL PRIMARY KEY,
--- empname TEXT NOT NULL, email NOT NULL);
-
--- INSERT INTO emp(empid, empname, email) VALUES
--- (1,"Eloise","Eloise@wit.edu");
--- INSERT INTO emp(empid, empname, email) VALUES
--- (2,"Aya","Aya@wit.edu");
-
-SELECT * from emp;
