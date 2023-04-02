2b88e7b0bd289eb29c243c7152d7bd74250c2c44
KiosoKen <batistar2@wit.edu>
2023-04-02 04:23:41 -0400

    feedback page updated

---
+++ feedbackSubmit.js
@@ -0,0 +1,12 @@
+document.getElementById("feedback-form").addEventListener("submit", function (event) {
+   event.preventDefault();
+   var form = event.target;
+   var formData = new FormData(form);
+   fetch('./submit.php', {
+      method: 'POST',
+      body: formData
+   })
+      .then(response => console.log(response.text()))
+      .then(data => console.log(data))
+      .catch(error => console.error(error))
+});
