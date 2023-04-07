f0716ac00a4ca3386b644ca378bd0784a233ffe6
KiosoKen <batistar2@wit.edu>
2023-04-07 12:59:20 -0400

    update

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const sqlite3 = require("sqlite3");
 const bodyParser = require("body-parser");
+const port = 3000;
+
 const app = express();
 const db = new sqlite3.Database(":memory:");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 db.serialize(() => {
     db.run(`CREATE TABLE form (
 email VARCHAR(255),
@@ -16,11 +21,15 @@ timeTaken TIME
 )`);
 });
 app.post("/submit-feedback", (req, res) => {
-    const { email, subject, feedback } = req.body;
+    const name = req.body.name;
+    const email = req.body.email;
+    const subject = req.body.subject;
+    const feedback = req.body.feedback;
+    console.log(`Received feedback: ${feedback}`);
     const dateTaken = new Date().toISOString().slice(0, 10);
     const timeTaken = new Date().toISOString().slice(11, 19);
     const stmt = db.prepare("INSERT INTO form (email, subject, feedback, dateTaken, timeTaken) VALUES (?, ?, ?, ?, ?)");
-    stmt.run(email, subject, feedback, dateTaken, timeTaken, (err) => {
+    stmt.run(name, email, subject, feedback, dateTaken, timeTaken, (err) => {
         if (err) {
             console.error(err);
             res.status(500).send("Error inserting feedback into the database.");
@@ -30,6 +39,6 @@ app.post("/submit-feedback", (req, res) => {
     });
     stmt.finalize();
 });
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
