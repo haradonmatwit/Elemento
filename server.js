e7af2741b2e178eba0b914102d0702eb696a0c56
phant9atwit <phant9@wit.edu>
2023-04-10 20:36:45 -0400

    More updates

--- server.js
+++ server.js
@@ -8,27 +8,35 @@ const db = new sqlite3.Database(":memory:");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 db.serialize(() => {
     db.run(`CREATE TABLE form (
-email VARCHAR(255),
-subject VARCHAR(15),
-feedback VARCHAR(255),
-dateTaken DATE,
-timeTaken TIME
-)`);
+        name VARCHAR(255),
+        email VARCHAR(255),
+        subject VARCHAR(15),
+        feedback VARCHAR(255),
+        dateTaken DATE,
+        timeTaken TIME
+    )`);
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Feedback.html');
+  });
+
 app.post("/submit-feedback", (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const subject = req.body.subject;
     const feedback = req.body.feedback;
-    console.log(`Received feedback: ${feedback}`);
+    console.log(`Received feedback from: ${name} <${email}>`);
+    console.log(`Subject: ${subject}`);
+    console.log(`${feedback}`);
     const dateTaken = new Date().toISOString().slice(0, 10);
     const timeTaken = new Date().toISOString().slice(11, 19);
-    const stmt = db.prepare("INSERT INTO form (email, subject, feedback, dateTaken, timeTaken) VALUES (?, ?, ?, ?, ?)");
+    const stmt = db.prepare("INSERT INTO form (name, email, subject, feedback, dateTaken, timeTaken) VALUES (?, ?, ?, ?, ?, ?)");
     stmt.run(name, email, subject, feedback, dateTaken, timeTaken, (err) => {
         if (err) {
             console.error(err);
@@ -39,6 +47,7 @@ app.post("/submit-feedback", (req, res) => {
     });
     stmt.finalize();
 });
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
